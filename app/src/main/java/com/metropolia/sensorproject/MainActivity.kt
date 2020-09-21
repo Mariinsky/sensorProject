11f3feaba285ab7dc557ef4fe7d7b1603852834e
Enar Mariinsky <enar.mariinsky@gmail.com>
2020-09-21 13:47:18 +0300

    Initial commit

---
+++ app/src/main/java/com/metropolia/sensorproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.metropolia.sensorproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
