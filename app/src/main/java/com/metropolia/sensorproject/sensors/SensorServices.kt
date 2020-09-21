cbdf8ae5587775aa0a3b6c1a8082f5f9c4b5f46d
Enar Mariinsky <enar.mariinsky@gmail.com>
2020-09-21 21:07:17 +0300

    Sensors data (#4)
    * location
    
    * linear acc and location

---
+++ app/src/main/java/com/metropolia/sensorproject/sensors/SensorServices.kt
@@ -0,0 +1,82 @@
+package com.metropolia.sensorproject.sensors
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Build
+import android.os.Looper
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+
+const val REQUEST_LOCATION_CODE = 100
+
+class SensorService(private val sensorManager: SensorManager) : SensorEventListener {
+
+    private val accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
+
+    fun registerListener() {
+        accSensor.also {
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+    override fun onSensorChanged(event: SensorEvent?) {
+        if(event?.sensor == accSensor) {
+            Log.i("XXX", event?.values?.get(0).toString())
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, value: Int) {
+
+    }
+}
+
+class LocationService(private val context: Context) {
+    private val locationClient = LocationServices.getFusedLocationProviderClient(context)
+    private val locationCallback = locationCallback()
+
+    fun startGettingLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            ActivityCompat.requestPermissions(context as Activity, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION ), REQUEST_LOCATION_CODE)
+            return
+        }
+        locationClient.requestLocationUpdates(
+            createLocationRequest(),
+            locationCallback,
+            Looper.getMainLooper()
+        )
+    }
+
+    fun stopLocationService() {
+        locationClient.removeLocationUpdates(locationCallback)
+    }
+
+    private fun locationCallback() = object : LocationCallback() {
+        @RequiresApi(Build.VERSION_CODES.O)
+        override fun onLocationResult(result: LocationResult?) {
+            result ?: return
+            for (location in result.locations) {
+                Log.i("XXX", "${location.longitude} ${location.latitude} ${location.speedAccuracyMetersPerSecond}")
+            }
+        }
+    }
+
+    private fun createLocationRequest(): LocationRequest? {
+        return LocationRequest.create()?.apply {
+            interval = 5000
+            fastestInterval = 1000
+            priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+        }
+    }
+}
