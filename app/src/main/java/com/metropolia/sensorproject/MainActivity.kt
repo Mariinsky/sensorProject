f3ef2d6eaee172e5f4801b8266a45bf84f47e787
Misty Bui <mishtybui@gmail.com>
2020-09-23 23:59:49 +0300

    input validate added

--- app/src/main/java/com/metropolia/sensorproject/MainActivity.kt
+++ app/src/main/java/com/metropolia/sensorproject/MainActivity.kt
@@ -12,14 +12,20 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import androidx.core.app.ActivityCompat
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     lateinit var sharedPreferences: SharedPreferences
     private val REQUEST_ALL_NEEDED_PERMISSIONS = 999
+    private val permissions =
+        arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACTIVITY_RECOGNITION,
+        )
 
     var isLogedIn = false
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,27 +40,41 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
         inputCheck()
-        btnSave.setOnClickListener{
-            val name: String = editTxtName.text.toString()
-            val weight: Int = editTxtWeight.text.toString().toInt()
-            val height: Int = editTxtHeight.text.toString().toInt()
-            val goal: Int = editTxtGoal.text.toString().toInt()
-            val editor: SharedPreferences.Editor = sharedPreferences.edit()
-            editor.putString("NAME", name)
-            editor.putInt("WEIGHT", weight)
-            editor.putInt("HEIGHT", height)
-            editor.putInt("GOAL", goal)
-            editor.putBoolean("CHECKBOX", true)
-            editor.apply()
+        btnSave.setOnClickListener(this)
 
-            Toast.makeText(this, "Saved", Toast.LENGTH_LONG).show()
-            val intent = Intent(this, StepTrackerActivity::class.java)
-            startActivity(intent)
-            finish()
+    }
+
+    private fun validate(): Boolean {
+        if(editTxtName.text.toString().isEmpty()){
+            layoutEditName.error = getString(R.string.input_empty)
+            return false
+        } else if (editTxtWeight.text.toString().isEmpty()){
+            layoutEditWeight.error = getString(R.string.input_empty)
+            return false
+        }else if (editTxtHeight.text.toString().isEmpty()) {
+            layoutEditHeight.error = getString(R.string.input_empty)
+            return false
+        }else if (editTxtGoal.text.toString().isEmpty()) {
+            layoutEditGoal.error = getString(R.string.input_empty)
+            return false
+        }else if (editTxtName.text.toString().length>20) {
+            layoutEditName.error = getString(R.string.name_input_error)
+            return false
+        }
+        return true
+    }
+
+    override fun onClick(p0: View?) {
+        when(p0?.id){
+            R.id.btnSave-> {
+                if(validate()){
+                    saveData()
+                }
+            }
         }
     }
+
     private fun inputCheck() {
-        //check length of name input
         editTxtName.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {}
 
@@ -65,16 +85,73 @@ class MainActivity : AppCompatActivity() {
                     Log.d("main","$count")
                     layoutEditName.error = getString(R.string.name_input_error)
                 } else {
-                    layoutEditName.isErrorEnabled = false
+                    layoutEditName.error = null
+                }
+            }
+        })
+        editTxtHeight.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (count == 0) {
+                    Log.d("main","$count")
+                    layoutEditHeight.error = getString(R.string.input_empty)
+                } else {
+                    layoutEditHeight.error = null
+                }
+            }
+        })
+        editTxtWeight.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (count == 0) {
+                    Log.d("main","$count")
+                    layoutEditWeight.error = getString(R.string.input_empty)
+                } else {
+                    layoutEditWeight.error = null
+                }
+            }
+        })
+        editTxtGoal.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (count == 0) {
+                    Log.d("main","$count")
+                    layoutEditGoal.error = getString(R.string.input_empty)
+                } else {
+                    layoutEditGoal.error = null
                 }
             }
         })
     }
-    private val permissions =
-        arrayOf(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACTIVITY_RECOGNITION,
-        )
+
+    private fun saveData() {
+        val name: String = editTxtName.text.toString()
+        val weight: Int = editTxtWeight.text.toString().toInt()
+        val height: Int = editTxtHeight.text.toString().toInt()
+        val goal: Int = editTxtGoal.text.toString().toInt()
+        //save data
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putString("NAME", name)
+        editor.putInt("WEIGHT", weight)
+        editor.putInt("HEIGHT", height)
+        editor.putInt("GOAL", goal)
+        editor.putBoolean("CHECKBOX", true)
+        editor.apply()
+
+        val intent = Intent(this, StepTrackerActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
 
     private fun checkPermissions(): Boolean {
         if (
