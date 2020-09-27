692fbe7c6629af3db6a31d622ef0c4c3f0ad3c0a
Misty Bui <mishtybui@gmail.com>
2020-09-27 22:25:57 +0300

    graph view but error when update data

--- app/src/main/java/com/metropolia/sensorproject/ProgressFragment.kt
+++ app/src/main/java/com/metropolia/sensorproject/ProgressFragment.kt
@@ -6,28 +6,26 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.github.mikephil.charting.animation.Easing.EaseOutBack
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.IAxisValueFormatter
+import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
 import com.metropolia.sensorproject.models.ProgressViewModel
-import com.metropolia.sensorproject.utils.animateTo
-import com.metropolia.sensorproject.utils.getFormattedDate
-import com.metropolia.sensorproject.utils.setBigMax
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-
 import kotlinx.android.synthetic.main.alert_dialog.view.*
-import kotlinx.android.synthetic.main.component_progress_bar.view.*
 import kotlinx.android.synthetic.main.fragment_progress.*
 import kotlinx.android.synthetic.main.fragment_progress.view.*
 import java.text.SimpleDateFormat
@@ -42,6 +40,7 @@ class ProgressFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -57,7 +56,7 @@ class ProgressFragment : Fragment() {
         val height = preferences.getInt("HEIGHT", 0)
         val weight = preferences.getInt("WEIGHT", 0)
 
-        rootView.floating_action_button.setOnClickListener{
+        rootView.floating_action_button.setOnClickListener {
             //Inflate the dialog and element with custom view
             val mDialogView = LayoutInflater.from(activity).inflate(R.layout.alert_dialog, null)
 
@@ -71,8 +70,8 @@ class ProgressFragment : Fragment() {
             val mBuilder = AlertDialog.Builder(activity)
                 .setView(mDialogView)
             //show dialog
-            val  mAlertDialog = mBuilder.show()
-            mDialogView.btnSave.setOnClickListener{
+            val mAlertDialog = mBuilder.show()
+            mDialogView.btnSave.setOnClickListener {
 
                 val newName: String = mDialogView.editTxtName.text.toString()
                 val newWeight: Int = mDialogView.editTxtWeight.text.toString().toInt()
@@ -94,22 +93,22 @@ class ProgressFragment : Fragment() {
                 } else if (editGoal.text.toString().isEmpty()) {
                     layoutGoal.error = getString(R.string.input_empty)
                 } else {*/
-                    //save data
-                    preferences =
-                        getActivity()!!.getSharedPreferences("SHARED_PREF", Context.MODE_PRIVATE)
-                    val editor: SharedPreferences.Editor = preferences.edit()
-                    editor.putString("NAME", newName)
-                    editor.putInt("WEIGHT", newWeight)
-                    editor.putInt("HEIGHT", newHeight)
-                    editor.putInt("GOAL", newGoal)
-                    editor.commit()
+                //save data
+                preferences =
+                    getActivity()!!.getSharedPreferences("SHARED_PREF", Context.MODE_PRIVATE)
+                val editor: SharedPreferences.Editor = preferences.edit()
+                editor.putString("NAME", newName)
+                editor.putInt("WEIGHT", newWeight)
+                editor.putInt("HEIGHT", newHeight)
+                editor.putInt("GOAL", newGoal)
+                editor.commit()
 
-                    val intent = Intent(activity, StepTrackerActivity::class.java)
-                    intent.putExtra("TabNumber", "1");
-                    startActivity(intent)
+                val intent = Intent(activity, StepTrackerActivity::class.java)
+                intent.putExtra("TabNumber", "1");
+                startActivity(intent)
 
             }
-            mDialogView.btnCancel.setOnClickListener{
+            mDialogView.btnCancel.setOnClickListener {
                 mAlertDialog.dismiss()
             }
         }
@@ -124,35 +123,60 @@ class ProgressFragment : Fragment() {
             .limitedActivitiesSubject
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe {
-                /*prgress_container.removeAllViews()
-                if (it.isNotEmpty()) {
-                    day_details.text = "${getFormattedDate(it.first().date)} ${it.first().Steps} steps"
-                } else {
-                    day_details.text = ""
+                barchart.invalidate(); // refresh
+
+                val entries = ArrayList<BarEntry>()
+                val days = ArrayList<String>()
+                //generate data for each bar and xaxis
+                it.forEach { day ->
+                    entries.add(BarEntry(entries.size.toFloat(), day.Steps.toFloat()))
                 }
-                    it.forEach { day ->
-                        val v = layoutInflater.inflate(R.layout.component_progress_bar, null) as LinearLayout
-                        v.date.text = SimpleDateFormat("EE", Locale.ENGLISH).format(day.date)
-                        v.progress_bar.setBigMax(goal)
-                        v.progress_bar.animateTo(day.Steps, 200)
-                        v.setOnClickListener {
-                            day_details.text = "${getFormattedDate(day.date)} ${day.Steps} steps"
-                        }
-                        prgress_container.addView(v)
-                    }*/
-                it.forEach {day ->
-                    val entries = ArrayList<BarEntry>()
-                    for(x in 0..7) entries.add(BarEntry(day.Steps.toFloat(),x.toFloat()))
-                    val barDataSet = BarDataSet(entries, "Steps")
-                    val labels = ArrayList<String>()
-                    labels.add(SimpleDateFormat("EE", Locale.ENGLISH).format(day.date))
-                    val data = BarData(barDataSet)
-                    data.setBarWidth(0.9f)
-                    barchart.data = data
-                    barchart.setFitBars(true); // make the x-axis fit exactly all bars
-                    barchart.invalidate(); // refresh// set the data and list of lables into chart
+                it.forEach { day ->
+                    days.add(SimpleDateFormat("MM-dd", Locale.ENGLISH).format(day.date))
                 }
 
+                val barDataSet = BarDataSet(entries, "Steps")
+                barDataSet.setColor(getResources().getColor(R.color.darkPink))
+
+                val dataSets = ArrayList<IBarDataSet>()
+                dataSets.add(barDataSet)
+
+                //set description
+                val description = Description()
+                description.setText("")
+                barchart.description = description
+
+                val data = BarData(barDataSet)
+                data.setBarWidth(0.9f)
+
+                //no data display
+                barchart.setNoDataText("No data")
+
+                //generate data for chart
+                barchart.data = data
+
+                barchart.setBackgroundColor(getResources().getColor(R.color.lightPink))
+                barchart.animateY(500, EaseOutBack);
+                barchart.setFitBars(true); // make the x-axis fit exactly all bars
+                barchart.animateY(5000)
+
+                //customize xAxis
+                val xAxis = barchart.getXAxis()
+                xAxis.setGranularity(1f); // minimum axis-step (interval) is 1
+                xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+                val xAxisFormatter = object : ValueFormatter() {
+                    override fun getFormattedValue(value: Float): String? {
+                        return days.get(value.toInt())
+                    }
+
+                    fun getDecimalDigits(): Int {
+                        return 0
+                    }
+                }
+                with(xAxis) {
+                    valueFormatter = xAxisFormatter
+                    setLabelCount(7)
+                }
             }
         viewModel.getLimitedActivities(7)
     }
